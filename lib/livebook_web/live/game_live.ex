3679beff6818128b67a2249dd9db65ecd60a13b9
Julio Barriuso <julio.barriuso@bluetab.net>
2023-03-14 17:43:39 +0100

    styles

--- lib/livebook_web/live/game_live.ex
+++ lib/livebook_web/live/game_live.ex
@@ -14,18 +14,57 @@ defmodule LivebookWeb.GameLive do
     ~H"""
     <h1>GAME LIVE</h1>
     <ul>
-      <li :for={{pid, %{pos: pos, name: name, target: target, status: status, score: score}} <- @board.players} ><%= inspect({name, pos, status, score}) %> -----> <%= inspect(target) %></li>
+      <li :for={{_pid, %{
+        pos: pos,
+        name: name,
+        target: target,
+        status: status,
+        score: score}} <- @board.players} >
+        <%= inspect({name, pos, status, score}) %> -----> <%= inspect(target) %>
+      </li>
     </ul>
 
     <div style={"width: #{GameEngine.Board.width()}px; height: #{GameEngine.Board.height()}px; border: 2px solid red; position: relative; top: 150px; left: 50px;"}>
-      <div :for={{pid, %{pos: {pos_x, pos_y}, name: name, target: target, status: status}} <- @board.players} id={inspect(pid)}
-        style={"position: absolute; top: #{pos_y - 8}px; left: #{pos_x - 8}px; width: 16px; height: 16px; border: 3px solid blue; border-radius: 50%;"}
-      ></div>
+      <div
+        :for={{pid, player} <- @board.players}
+        id={inspect(pid)}
+        style={player_style(player)}
+      ><%= player.name %></div>
 
-      <div :for={{{x, y}, size} <- @board.fruits} id={"fruit-#{x}-#{y}"}
-        style={"position: absolute; top: #{y - size / 2}px; left: #{x - size / 2}px; width: #{size}px; height: #{size}px; border: 4px solid gold; border-radius: 50%;"}
+      <div :for={{{x, y}, _} = fruit <- @board.fruits} id={"fruit-#{x}-#{y}"}
+        style={fruit_style(fruit)}
       ></div>
     </div>
+    """
+  end
+
+  defp player_style(%{pos: {pos_x, pos_y}, size: size, color: color}) do
+    """
+      position: absolute;
+      top: #{pos_y - size / 2}px;
+      left: #{pos_x - size /2}px;
+      width: #{size}px;
+      height: #{size}px;
+      border: #{size/5}px solid #{color};
+      border-radius: 50%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      font-size: #{2*(size/3)}px;
+      -webkit-text-stroke: #{size/50}px #FFF;
+    """
+  end
+
+  defp fruit_style({{x, y}, size}) do
+    """
+      position: absolute;
+      top: #{y - size / 2}px;
+      left: #{x - size / 2}px;
+      width: #{size}px;
+      height: #{size}px;
+      border: 4px solid gold;
+      border-radius: 50%;
     """
   end
 
